a2a66c7f315a8ff564524abcf98c1ac038dc4739
Ashok Jaat <153806900+ASHOKJAAT12@users.noreply.github.com>
2026-08-30 11:22:44 +0530

    Time: 0 ms (100%), Space: 92.2 MB (4.89%) - LeetHub

--- 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.cpp
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.cpp
@@ -1,30 +1,30 @@
-#include<math.h>
 class Solution {
 public:
     int minimumDeletions(vector<int>& nums) {
-        int minm = nums[0];
-        int min_pos = 0;
-        int max = nums[0];
-        int max_pos = 0;
         int n = nums.size();
-        for ( int  i = 1 ; i < n ; i++ ) {
-            if ( minm > nums[i] ) {
-                minm = nums[i];
-                min_pos = i;
+
+        int minIndex = 0;
+        int maxIndex = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (nums[i] < nums[minIndex]) {
+                minIndex = i;
             }
-            if ( max < nums[i] ) {
-                max = nums[i];
-                max_pos = i;
+
+            if (nums[i] > nums[maxIndex]) {
+                maxIndex = i;
             }
         }
 
-        if ( min_pos > max_pos ) {
-            swap(min_pos,max_pos);
-        }
-         int left = max_pos + 1;
-        int right = n - min_pos;
-        int both = (min_pos + 1) + (n - max_pos);
+        int removeFromFront = max(minIndex, maxIndex) + 1;
 
-        return min({left, right, both});
+        int removeFromBack = n - min(minIndex, maxIndex);
+
+        int removeFromBothSides = min(
+            minIndex + 1 + (n - maxIndex),
+            maxIndex + 1 + (n - minIndex)
+        );
+
+        return min(removeFromFront, min(removeFromBack, removeFromBothSides));
     }
 };
